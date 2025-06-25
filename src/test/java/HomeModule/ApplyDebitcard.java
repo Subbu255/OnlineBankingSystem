520faa7a541823a7f00d4642657baef1c540f533
Subbu255 <128217060+Subbu255@users.noreply.github.com>
2025-06-25 08:03:48 +0530

    Update ApplyDebitcard.java

--- src/test/java/HomeModule/ApplyDebitcard.java
+++ src/test/java/HomeModule/ApplyDebitcard.java
@@ -1,8 +1,16 @@
 package HomeModule;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
 public class ApplyDebitcard 
+
+	WebDriver driver;
+
 {
 @Test
 public void debit()
@@ -15,4 +23,27 @@ public void credit()
 	System.out.println("credit card is applied ");
 	System.out.println("credit card is applied ");
 	}
+
+	@BeforeClass
+    public void setup() {
+        // Set the path to your ChromeDriver if not in system PATH
+        // System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testGoogleSearch() {
+        driver.get("https://www.google.com");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium WebDriver");
+        searchBox.submit();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
